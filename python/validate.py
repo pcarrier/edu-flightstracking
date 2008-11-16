eda9a8f7dafef74ad3aa48b16a4e0869dafbdfe7
Jean-Christophe Saad-Dupuy <saad.dupuy@yahoo.fr>
2008-11-16 19:33:44 +0100

    Added a rng validation script using libxml in python.

---
+++ python/validate.py
@@ -0,0 +1,38 @@
+"""
+This use the relax-ng file given to validate the xml file given.
+"""
+
+import lxml
+from lxml import etree
+import os,sys
+
+def Validate(xml_file, rng_file):
+    if os.path.exists(xml_file):
+        if os.path.exists(rng_file):
+            xml_doc=etree.parse(xml_file)
+            rng_doc=etree.parse(rng_file)
+            
+            relaxng=etree.RelaxNG(rng_doc)
+            #print xml_doc.relaxng(rng_doc)
+            print relaxng.validate(xml_doc)
+            if relaxng.validate(xml_doc):
+                print "Validating SUCCESSFULL"
+            else:
+                log=relaxng.error_log
+                raise Exception, log.last_error
+                
+            
+        else:
+            raise Exception,"FATAL : %s doesn't exixts"%xsl_file
+    else:
+        raise Exception, "FATAL : %s doesn't exixts"%file_in
+
+
+if __name__=="__main__":
+    #TODO : Parsing argv with getopt
+    file_in=sys.argv[1]
+    rng_file=sys.argv[2]
+    
+    print "Validating %s with rng file %s :)"%(file_in, rng_file)
+    Validate(file_in, rng_file)
+    
