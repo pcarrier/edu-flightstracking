bc286c4431ec5315e678661d58115bd47fb5ec78
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2008-12-13 18:24:46 +0100

    Google Maps !

--- web/main.js
+++ web/main.js
@@ -1,1 +1,8 @@
 alert("Version beta !");
+
+function loadMap() {
+    if (GBrowserIsCompatible()) {
+        var map = new GMap2(document.getElementById("map"));
+        map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+    }
+}
