362ae04efc4299f2eedf1ab895824cc3a5ed4e5c
Geoffroy Carrier <geoffroy.carrier@koon.fr>
2008-12-14 16:45:44 +0100

    Travaillons a 2 !

--- web/main.js
+++ web/main.js
@@ -1,8 +1,8 @@
-alert("Version beta !");
+//alert("Version beta !");
 
 function loadMap() {
     if (GBrowserIsCompatible()) {
         var map = new GMap2(document.getElementById("map"));
-        map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+        map.setCenter(new GLatLng(46.316, 3.1640), 5);
     }
 }
